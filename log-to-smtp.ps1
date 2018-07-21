2ad0b6bb6b3de4c31ecdfe5be6bbdfc7179c5e9d
zuphzuph <zuphzuph@users.noreply.github.com>
2018-07-20 22:42:48 -0600

    Commit
    Mang.

---
+++ log-to-smtp.ps1
@@ -0,0 +1,28 @@
+stop-service "Full Service Name."
+Copy-Item "Path to Debugger File." -Destination "Save Debug File To."
+Clear-Content "Clear Log File."
+start-service "Full Service Name."
+
+$Username = "domain/acct";
+$Password = "cred";
+$path = "Path of Log to Send.";
+
+function Send-ToEmail([string]$email, [string]$attachmentpath){
+
+    $message = new-object Net.Mail.MailMessage;
+    $message.From = "account@smtpdomain.com";
+    $message.To.Add($email);
+    $message.Subject = "Describe Sent Log";
+    $message.Body = "";
+    $attachment = New-Object Net.Mail.Attachment($attachmentpath);
+    $message.Attachments.Add($attachment);
+
+    $smtp = new-object Net.Mail.SmtpClient("stmp.mail.com");
+    $smtp.EnableSSL = $true;
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { return $true }
+    $smtp.send($message);
+    write-host "Mail Sent" ; 
+    $attachment.Dispose();
+ }
+Send-ToEmail  -email "destination@domain.com" -attachmentpath $path;
